9819caeea2412be9b152c87094ae3ae8a130fe30
naman srivastav <namansrivastav@namans-MacBook-Air.local>
2026-01-21 16:21:27 +0530

    Question 1 solution - File Operations

---
+++ q1/app.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+fs.readFile("input.txt", "utf8", (err, data) => {
+    if (err) {
+        console.log("Error reading file");
+        return;
+    }
+    const words = data.trim().split(/\s+/);
+    const wordCount = words.length;
+    const result = `Total words: ${wordCount}`;
+    fs.writeFile("output.txt", result, (err) => {
+        if (err) {
+            console.log("Error writing file");
+            return;
+        }
+        console.log("Word count written to output.txt");
+    });
+});
